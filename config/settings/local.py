aa71f51f20c57ca465894c1478cbbc8496866621
dimsplendid <dimsplendid@gmail.com>
2022-02-23 00:32:39 +0800

    setting notebook configurations

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,4 @@
+import os
 from .base import *  # noqa
 from .base import env
 
@@ -59,6 +60,8 @@ INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ["django_extensions"]  # noqa: F405
+SHELL_PLUS_PRINT_SQL = True
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true" # only use in development 
 
 # Your stuff...
 # ------------------------------------------------------------------------------
