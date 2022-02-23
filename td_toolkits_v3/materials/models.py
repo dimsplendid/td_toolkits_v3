4e25989065befbab38ebbf262c9dbefc6df33df1
dimsplendid <dimsplendid@gmail.com>
2022-02-23 21:42:53 +0800

    Add LiquidCrystal model

--- td_toolkits_v3/materials/models.py
+++ td_toolkits_v3/materials/models.py
@@ -1,15 +1,31 @@
-from autoslug import AutoSlugField
 from django.db import models
 
+from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
+
 
 class Vender(TimeStampedModel):
     name = models.CharField("Name of Vender", max_length=255)
-    slug = AutoSlugField("Vender Adress")
+    slug = AutoSlugField(
+        "Vender Adress",
+        unique=True, always_update=False, populate_from="name")
+
+    def __str__(self):
+        return self.name
+
+
+def get_default_vender():
+    return Vender.objects.get_or_create(name="INX")[0]
+
 
 class LiquidCrystal(TimeStampedModel):
     name = models.CharField("Name of Liquid Crystal", max_length=255)
     slug = AutoSlugField(
         "LC Address",
         unique=True, always_update=False, populate_from="name")
-    
+    vender = models.ForeignKey(
+        Vender,
+        default=get_default_vender, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
