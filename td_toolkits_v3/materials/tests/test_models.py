98f8de2c398baca7e80f7b1fd7f944c0d094331c
dimsplendid <dimsplendid@gmail.com>
2022-02-24 23:52:39 +0800

    LiquidCrystal model test pass

--- td_toolkits_v3/materials/tests/test_models.py
+++ td_toolkits_v3/materials/tests/test_models.py
@@ -1,26 +1,28 @@
-import pytest
-
-# Connects our tests with our database
-pytestmark = pytest.mark.django_db
-
+from .factories import (
+    VenderFactory,
+    LiquidCrystalFactory
+)
 from ..models import (
     Vender,
     get_default_vender,
     LiquidCrystal,
 )
-from .factories import (
-    VenderFactory,
-    LiquidCrystalFactory
-)
+import pytest
+
+# Connects our tests with our database
+pytestmark = pytest.mark.django_db
+
 
 @pytest.fixture
 def lc():
     return LiquidCrystalFactory()
 
+
 @pytest.fixture
 def lc_real():
     return LiquidCrystal.objects.create(
         name='LCT-15-1098',
+        designed_cell_gap=2.8,
         n_e=1.5794,
         n_o=1.4818,
         e_para=3.5,
@@ -36,24 +38,52 @@ def test_vender__str__():
     assert vender.__str__() == vender.name
     assert str(vender) == vender.name
 
+
 def test_liquid_crystal__str__(lc):
     assert lc.__str__() == lc.name
     assert str(lc) == lc.name
+
 
 def test_get_default_vender():
     vender = get_default_vender()
     assert vender.__str__() == "INX"
     assert str(vender) == "INX"
 
+
 def test_lc_default_vender():
     lc = LiquidCrystal.objects.create(name='LCT-15-1098')
     assert lc.vender.name == 'INX'
+
 
 def test_lc_get_absolute_url(lc):
     url = lc.get_absolute_url()
     assert url == f'/materials/lcs/{lc.slug}/'
 
+
 def test_lc_delta_n(lc, lc_real):
-    
     assert lc.delta_n is None
     assert lc_real.delta_n == pytest.approx(0.0976)
+
+
+def test_lc_delta_e(lc, lc_real):
+    assert lc.delta_e is None
+    assert lc_real.delta_e == pytest.approx(-3.4)
+
+
+def test_lc_retardation(lc, lc_real):
+    assert lc.retardation is None
+    assert lc_real.retardation == lc_real.delta_n \
+        * lc_real.designed_cell_gap \
+        * 1000
+
+def test_lc_scatter_index(lc, lc_real):
+    assert lc.scatter_index is None
+    assert lc_real.scatter_index == pytest.approx(0.007208509601712038)
+
+def test_lc_response_index(lc, lc_real):
+    assert lc.response_index is None
+    assert lc_real.response_index == pytest.approx(14.557823129251702)
+
+def test_lc_vt_index(lc, lc_real):
+    assert lc.vt_index is None
+    assert lc_real.vt_index == pytest.approx(0.23129251700680276)
