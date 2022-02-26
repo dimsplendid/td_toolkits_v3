adf5bb3a331087db91ebc42fa19a878e1b11eee1
dimsplendid <dimsplendid@gmail.com>
2022-02-26 21:58:34 +0800

    add test for products' __str__() methods, test pass

---
+++ td_toolkits_v3/products/tests/factories.py
@@ -0,0 +1,55 @@
+from django.template.defaultfilters import slugify
+
+
+import factory
+import factory.fuzzy
+import pytest
+
+
+from ..models import (
+    ProductModelType,
+    Sub,
+    Chip
+)
+
+@pytest.fixture
+def product_model_type():
+    return ProductModelTypeFactory()
+
+@pytest.fixture
+def sub():
+    return SubFactory()
+
+@pytest.fixture
+def chip():
+    return ChipFactory()
+
+class ProductModelTypeFactory(factory.django.DjangoModelFactory):
+    name = factory.fuzzy.FuzzyText()
+    slug = factory.LazyAttribute(
+        lambda obj: slugify(obj.name)
+    )
+
+    class Meta:
+        model = ProductModelType
+
+class SubFactory(factory.django.DjangoModelFactory):
+    name = factory.fuzzy.FuzzyText()
+    slug = factory.LazyAttribute(
+        lambda obj: slugify(obj.name)
+    )
+    product_type = factory.SubFactory(ProductModelTypeFactory)
+
+    class Meta:
+        model = Sub
+
+class ChipFactory(factory.django.DjangoModelFactory):
+    name = factory.fuzzy.FuzzyText()
+    slug = factory.LazyAttribute(
+        lambda obj: slugify(obj.name)
+    )
+    sub = factory.SubFactory(SubFactory)
+    product_type = factory.SubFactory(ProductModelTypeFactory)
+
+    class Meta:
+        model = Chip
--- td_toolkits_v3/products/tests/test_models.py
+++ td_toolkits_v3/products/tests/test_models.py
@@ -1,9 +1,26 @@
 import pytest
 
 from ..models import ProductModelType
+from .factories import (
+    product_model_type,
+    sub,
+    chip,
+)
 
 pytestmark = pytest.mark.django_db
 
 def test_product_model_type_name_default():
     product = ProductModelType.default()
     assert product.name == '5905'
+
+def test_product_model_type__str__(product_model_type):
+    assert product_model_type.__str__() == product_model_type.name
+    assert str(product_model_type) == product_model_type.name
+
+def test_sub__str__(sub):
+    assert sub.__str__() == sub.name
+    assert str(sub) == sub.name
+
+def test_chip__str__(chip):
+    assert chip.__str__() == chip.name
+    assert str(chip) == chip.name
