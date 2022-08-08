eb0113c85aa46fb2f44c4c9211335c40c4f6e16a
dimsplendid <dimsplendid@gmail.com>
2022-08-08 15:13:44 +0800

    Add Chip upload success view

--- td_toolkits_v3/products/views.py
+++ td_toolkits_v3/products/views.py
@@ -1,12 +1,14 @@
-import imp
-from re import template
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
-    CreateView,
+    TemplateView
 )
 from django.views.generic.edit import FormView
+from django.core.cache import cache
+
+import pandas as pd
+
 from .models import (
     Chip,
 )
@@ -19,7 +21,7 @@ class ChipListView(ListView):
 class ChipsBatchCreateView(LoginRequiredMixin, FormView):
     template_name = 'form_generic.html'
     form_class = ChipsUploadForm
-    success_url = reverse_lazy('products:chip_list')
+    success_url = reverse_lazy('products:chip_upload_success')
 
     def form_valid(self, form):
         form.save()
@@ -31,4 +33,28 @@ class ChipsBatchCreateView(LoginRequiredMixin, FormView):
         context['file_path'] = reverse_lazy('materials:template') \
                              + '?download=optical_condition_upload_template'
 
+        return context
+    
+class ChipBatchCreateSuccessView(TemplateView):
+    template_name = 'success_generic.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Chips Upload Success'
+        df: pd.DataFrame = cache.get('df')
+        context['log'] = cache.get('save_log')
+        try:
+            context['table'] = df.to_html(
+                float_format=lambda x: f'{x:.4f}',
+                classes='table table-striped table-bordered table-hover',
+                justify='center',
+                index=False,
+                escape=False,
+            )
+        except:
+            context['table'] = None
+        context['nexts'] = {
+            "RDL Upload": reverse_lazy('opticals:rdl_cell_gap_upload'),
+            "AXO Upload": reverse_lazy('opticals:axo_upload'),
+        }
         return context
