f0046e17d7185edd19615f72d3bae55cd85e7f73
andrew.tseng <andrew.tseng@innolux.com>
2023-02-03 17:00:07 +0800

    Temp work at office

--- jupyter_notes/utils/preprocessing.py
+++ jupyter_notes/utils/preprocessing.py
@@ -10,12 +10,24 @@ class PreprocessingResult:
     def __iter__(self):
         return iter((self.data, self.fail_ids))
 
-def vt_fail_drop(
+def vt_preprocessing(
     df: pd.DataFrame,
     vop_key: str = 'Vop',
     lc_efficiency_key: str = 'LC%',
     
 ) -> PreprocessingResult:
     """
+    remove the failure measurement data base on VT curve and recent knowledge
+    1. 
+    2. 
+    
+    Parameters
+    ==========
+    
+    
+    Returns
+    =======
     """
     pass
+
+def rt_preprocessing
