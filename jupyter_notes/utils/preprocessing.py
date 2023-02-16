de0bd311d67968940841189fa7db897fc0b6f38e
andrew.tseng <andrew.tseng@innolux.com>
2023-02-16 09:38:55 +0800

    Finish vt preprocessing function

--- jupyter_notes/utils/preprocessing.py
+++ jupyter_notes/utils/preprocessing.py
@@ -1,31 +1,44 @@
 from __future__ import annotations
-from typing import Union, Any
-import numpy as np
+from typing import Callable
 import pandas as pd
 from dataclasses import dataclass, field
 
 @dataclass
 class PreprocessingResult:
     data: pd.DataFrame = field(default_factory=pd.DataFrame)
-    fail_ids: list[str] = field(default_factory=list)
+    fail_ids: set[str] = field(default_factory=set)
     
     def __iter__(self):
         return iter((self.data, self.fail_ids))
 
 def is_smooth(
     data: pd.Series[float],
+    cut_off: float = 0.01,
+) -> bool:
+    """
+    Using the standard deviation of difference to justify the data is smooth 
+    or not.
     
-):
-    ...
+    ## Parameters
+    data: pd.Series[float]
+        the data series to be justify
+    cut_off: float = 0.01
+        the cut off to judge the smooth data, 
+    """
+    diff = data.diff()
+    std: float = diff.std() # type: ignore
+    return (std < cut_off)
 
 def vt_preprocessing(
     data: pd.DataFrame,
     id_key: str = 'ID',
+    point_key: str = 'Point',
     vop_key: str = 'Vop',
     lc_efficiency_key: str = 'LC%',
     normal_white_vop: float = 4.8,
     lc_efficency_cut_off: float = 0.5,
-    other_fail_ids: list[str]|None = None,
+    other_fail_ids: list[str] | set[str] | None = None,
+    is_smooth_f: Callable[[pd.Series[float]], bool] = is_smooth,
 ) -> PreprocessingResult:
     """
     remove the failure measurement data base on VT curve and recent knowledges
@@ -59,15 +72,35 @@ def vt_preprocessing(
         all the fail IDs
     """
     result = PreprocessingResult()
-    result.fail_ids += data[
+    result.fail_ids = result.fail_ids | set(data[
         (data[vop_key]==normal_white_vop) &
         (data[lc_efficiency_key] < lc_efficency_cut_off)
-    ][id_key].unique().tolist() # type: ignore
+    ][id_key].unique().tolist())                           # type: ignore
+    
+    tmp_df = data[
+        data[vop_key] > normal_white_vop
+    ][[id_key, point_key, lc_efficiency_key]].copy()       # type: ignore
+    tmp_df['Sample'] = ( 
+          tmp_df[id_key].astype(str)                       # type: ignore
+        + '-' 
+        + tmp_df[point_key].astype(str)                    # type: ignore
+    )
+    tmp_df: pd.DataFrame = (
+        tmp_df[['Sample', lc_efficiency_key]]              # type: ignore
+        .groupby('Sample')
+        .apply(is_smooth_f)                                # type: ignore
+    )
+    
+    result.fail_ids = result.fail_ids | {
+        ele.split('-')[0]                                  # type: ignore
+        for ele in                                         # type: ignore
+        tmp_df[~tmp_df[lc_efficiency_key]].index.to_list() # type: ignore     
+    }
     
     if other_fail_ids is not None:
-        result.fail_ids += other_fail_ids
+        result.fail_ids = result.fail_ids | set(other_fail_ids)
     
-    
+    result.data = data[~data['ID'].isin(result.fail_ids)]  # type: ignore
 
     
     return result
