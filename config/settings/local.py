ed98700c3b3dbef067fbe7aa0272d6ecace457be
andrew.tseng <andrew.tseng@innolux.com>
2023-09-13 10:55:10 +0800

    modify local ip

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,7 @@ SECRET_KEY = env(
     default="!!!SET DJANGO_SECRET_KEY!!!",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "10.53.13.143"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "10.53.13.106"]
 
 # CACHES
 # ------------------------------------------------------------------------------
